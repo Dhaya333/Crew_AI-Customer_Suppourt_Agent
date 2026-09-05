1619344e8f2445091f3c298cf2a40ed30d330370
Dhaya333 <udhyaarumugam333@gmail.com>
2026-09-05 06:57:56 +0530

    Updated tool functions

--- src/customer_support/tools/custom_tools.py
+++ src/customer_support/tools/custom_tools.py
@@ -1,8 +1,4 @@
-"""
-Custom / configured tools for the Customer Support Crew.
-"""
-
-from crewai_tools import ScrapeWebsiteTool
+from crewai_tools import ScrapeWebsiteTool, SerperDevTool
 
 # Scrapes the CrewAI docs page so the support agent can ground its
 # answers in real documentation instead of hallucinating.
@@ -10,9 +6,5 @@ docs_scrape_tool = ScrapeWebsiteTool(
     website_url="https://docs.crewai.com/how-to/Creating-a-Crew-and-kick-it-off/"
 )
 
-# Add more tools here as needed, e.g.:
-#
-# from crewai_tools import SerperDevTool, WebsiteSearchTool
-#
-# search_tool = SerperDevTool()
-# website_search_tool = WebsiteSearchTool()
+
+search_tool = SerperDevTool()
