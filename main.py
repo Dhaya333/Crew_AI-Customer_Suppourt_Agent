39822c3be25a040518bc6d0d46969690cf748f32
Dhaya333 <udhyaarumugam333@gmail.com>
2026-09-07 20:33:51 +0530

    Main file commit

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ from customer_support.crew import CustomerSupportCrew
 def run():
     """Kick off the customer support crew with a sample inquiry."""
     inputs = {
-        "customer": "DeepLearningAI",
-        "person": "Udhaya",
+        "customer": "soniya sharma",
+        "person": "Gobiiii",
         "inquiry": (
             "I need help with setting up a Crew and kicking it off, "
             "specifically how can I add memory to my crew? "
